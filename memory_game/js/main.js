e585e6d42bf8964c83259dba0370dd884f152651
Nathan Kjos <nathankjos@Nathans-MacBook-Pro.local>
2018-01-10 16:41:26 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,25 @@
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped " + cardOne);
-console.log("User Flipped " + cardTwo);
-if (cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You've Found a match!");
-	}else{
-		alert("Sorry, try again.");
+
+var checkForMatch = function(){
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+	} else {
+	console.log("Sorry, try again.");
 	}
 }
+
+var flipCard = function(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert("You've Found a match!");
+		}else{
+			alert("Sorry, try again.");
+		}
+	}
+}
+flipCard(0);
+flipCard(2);
